83eabcb2282c1e728af4c19af8bea020840cffe0
Shubhashree359 <142501263+Shubhashree359@users.noreply.github.com>
2023-09-01 10:13:17 -0700

    Create jc_tb.v

---
+++ jc_tb.v
@@ -0,0 +1,22 @@
+module pes_jc_tb;
+reg clk;
+reg rst;
+wire [7:0]q;
+pes_jc j1(rst,clk,q);
+
+initial
+begin
+clk=0;
+rst = 0;
+
+//$monitor($time, ,”c=%b”,clk, , ,”r=%b”,rst, , ,”q=%b”,q);
+  $dumpfile("pes_jc.vcd");
+$dumpvars(1);
+#6 rst =1;
+end
+
+always #2 clk = ~clk;
+
+initial #68 $finish;
+
+endmodule
